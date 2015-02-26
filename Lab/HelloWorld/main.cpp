2c970a1cdf8878faac7a314343d9715dfe45db4e
unknown <malopez16patito@hotmail.com>
2015-02-25 20:54:14 -0800

    Signed-off-by: MarioLopez250 <malopez16patito@hotmail.com>

---
+++ Lab/HelloWorld/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Mario
+ *
+ * Created on February 25, 2015, 8:41 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
---
+++ Lab/Lab021815/HelloWorld/main.cpp
@@ -0,0 +1,28 @@
+/* 
+ * File:   main.cpp
+ * Author: Mario Lopez
+ *
+ * Created on February 18, 2015, 9:55 AM
+ *      Purpose: Our first program!
+ */
+
+//System Libraries 
+#include <iostream> //Input/Output Library 
+
+
+//User Libraries
+
+//Global constants 
+
+//Function Prototypes 
+
+//Execution begins here!
+
+int main(int argc, char** argv) {
+    //Output Hello World
+    std::cout<<"Hello World"<<std::endl;
+    //Exit stage right!
+    
+    return 0;
+}
+
