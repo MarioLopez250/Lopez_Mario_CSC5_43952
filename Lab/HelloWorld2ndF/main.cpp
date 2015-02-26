5c65cbb254ec05998bfeabe10b92c45a7d14cfac
unknown <malopez16patito@hotmail.com>
2015-02-25 19:58:04 -0800

    Signed-off-by: unknown <malopez16patito@hotmail.com>

---
+++ Lab/HelloWorld2ndF/main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.cpp
+ * Author: Mario
+ *
+ * Created on February 25, 2015, 7:56 PM
+ *   Purpose:  Wasn't satisfied with 1 F
+ *              I wanted another
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
---
+++ Lab/HelloWorldGradeF/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Mario
+ *
+ * Created on February 25, 2015, 7:54 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
