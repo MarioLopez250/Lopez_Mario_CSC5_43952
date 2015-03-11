cd08d2087a591eb49a431806d2aeab5ab6350045
MarioLopez250 <mlopez250@student.rcc.edu>
2015-03-11 10:57:47 -0700

    Signed-off-by: MarioLopez250 <mlopez250@student.rcc.edu>

---
+++ Hmwrk/Assignment2/Gaddis_Chap3_PaymentProb/main.cpp
@@ -0,0 +1,23 @@
+/* 
+ * File:   main.cpp
+ * Author: Mario Lopez
+ *
+ * Created on March 11, 2015, 10:40 AM
+ *      Purpose: Gaddis_Chap3_PaymentProb
+ */
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    //temp=pow((1+r),N)
+    //temp=static_cast<float>(pow(l*1+r)n);
+    //F=1.8*c+32
+    
+    return 0;
+}
+
