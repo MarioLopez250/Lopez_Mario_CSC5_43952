584eaab190576cc59551cb8492500db12dd9da50
MarioLopez250 <mlopez250@student.rcc.edu>
2015-03-25 10:56:50 -0700

    Signed-off-by: MarioLopez250 <mlopez250@student.rcc.edu>

--- Hmwrk/Assignment3/Gaddis_Chapter4_Prob1/main.cpp
+++ Hmwrk/Assignment3/Gaddis_Chapter4_Prob1/main.cpp
@@ -4,6 +4,7 @@
  *
  * Created on March 23, 2015, 10:19 AM
  *      Purpose: Gaddis Chapter 4 Problem 1
+ *              Minimum/Maximum
  * 
  */
 
@@ -19,6 +20,14 @@ using namespace std;
 
 //Execution Begins Here!!!
 int main(int argc, char** argv) {
+    
+    //Declare Variables
+    float num1, num2;
+    cout<<"Enter Two Numbers"<<endl;
+    cin>>num1;
+    cin>>num2;
+    num1>num2?cout<<num1<<" is greater than "
+            <<num2:cout<<num1<<" is smaller than "<<num2;
     
     
     return 0;
---
+++ Hmwrk/Assignment3/Gaddis_Chapter4_prob2/main.cpp
@@ -0,0 +1,36 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on March 25, 2015, 10:49 AM
+ * Purpose:Gaddis Chapter 4 Problem 2
+ *           Roman Numeral Converter
+ * 
+ */
+
+//System Library
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!!!
+int main(int argc, char** argv) {
+    //Declare Variables
+    unsigned short num;
+    //Prompt the user for inputs
+    cout<<"Enter a number from 1-10 to convert it to Roman Numerals"<<endl;
+    cin>>num;
+    if (num>=1&&<=10){
+        
+    }
+        
+    
+
+    return 0;
+}
+
