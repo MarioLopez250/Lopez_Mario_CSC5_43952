607628e57ac00246083b12ce17f45ea06514070a
MarioLopez250 <mlopez250@student.rcc.edu>
2015-04-06 10:53:45 -0700

    Signed-off-by: MarioLopez250 <mlopez250@student.rcc.edu>

---
+++ Hmwrk/Assignment4/Chapter5_Gaddis_8thEd_Prob1/main.cpp
@@ -0,0 +1,57 @@
+/* 
+ * File:   main.cpp
+ * Author: Mario Lopez
+ *      Purpose: Chapter 5 Gaddis Problem 1 
+ *              Sum of Numbers
+ *
+ * Created on April 6, 2015, 10:28 AM
+ */
+
+//System Libraries
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare Variables 
+    float num;
+        
+    //Prompt the user for input
+    cout<<"Enter a positive integer value, "<<endl
+        <<"and I will find the sum of all integer "
+        <<"numbers from 1 to the number entered"<<endl;
+    cin>>num;
+    
+    do{
+   if (num>0)
+ {
+  float sumNum;
+       
+ cout<<"The sum of all integers numbers from 1 to "<<num<<" is"<<sumNum<<endl;
+ }
+ 
+ else
+     cout<<"Enter a positive integer value"<<endl;
+ 
+ }while (num<num); 
+   
+
+/*Write a program that asks the user for a positive integer value.
+ *  The program should use
+ * a loop to get the sum of all the integers from 1 up to the number entered. 
+ * For example,
+ * if the user enters 50, the loop will find the sum of 1, 2, 3, 4, … 50.
+ * Input Validation: Do not accept a negative starting number.
+*/
+    return 0;
+}
+
