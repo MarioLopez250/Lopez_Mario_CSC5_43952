0711fecb9963e3f313aac6ff06c25914fbe273b4
MarioLopez250 <mlopez250@student.rcc.edu>
2015-04-29 10:53:35 -0700

    Signed-off-by: MarioLopez250 <mlopez250@student.rcc.edu>

---
+++ Lab/Craps_V1/main.cpp
@@ -0,0 +1,29 @@
+/* 
+ * File:   main.cpp
+ * Author: Mario Lopez
+ * Purpose:
+ *
+ * Created on April 29, 2015, 10:27 AM
+ */
+
+//System Libraries
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <ctime>
+
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!!!
+int main(int argc, char** argv) {
+    
+
+    return 0;
+}
+
---
+++ Lab/Infinite_Exponential_Sequence/main.cpp
@@ -0,0 +1,30 @@
+/* 
+ * File:   main.cpp
+ * Author: Mario Lopez
+ * Purpose:
+ *
+ * Created on April 29, 2015, 8:02 AM
+ */
+
+//System Libraries
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <ctime>
+
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!!!
+int main(int argc, char** argv) {
+    
+    
+
+    return 0;
+}
+
