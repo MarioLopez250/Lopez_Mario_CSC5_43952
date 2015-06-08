b0096ac2611d72c7b04c4bee84e4793b2cfa2250
MarioLopez250 <mlopez250@student.rcc.edu>
2015-06-08 09:22:01 -0700

    Signed-off-by: MarioLopez250 <mlopez250@student.rcc.edu>

--- Projects/Project2/Project1_V2/main.cpp
+++ Projects/Project2/Project1_V2/main.cpp
@@ -6,22 +6,17 @@
  * Created on May 20, 2015, 9:56 AM
  */
 
-#include <algorithm>
-#include <vector>
-#include <bitset>
-
-
 //System Libraries
-#include <iostream>
-#include <cstdlib>
-#include <iomanip>
-#include <fstream>
-#include <cmath>
-#include <ctime>
+#include <iostream>//I/O standard //some stream manipulators
+#include <cstdlib>//C-style random numbers Random Number generator 
+#include <iomanip>//Formatting //standard streams cin/cout //input and output
+#include <fstream>//I/O File //Reading/Writing from a file/files to/from streams
+#include <cmath>//
+#include <ctime>//
 
-#include <algorithm>
-#include <vector>
-#include <bitset>
+#include <algorithm>//general algorithms
+#include <vector>//one-dimensional arrays
+
 using namespace std;
 
 //User Libraries
@@ -31,9 +26,14 @@ using namespace std;
 //Function Prototypes
 void filAray(vector<int> &,int);
 void prntAry(vector<int> &,int,int);
-void markSrt(vector<int> &,int);
+void mSrt(vector<int> &,int);
 int linSrch(vector<int> &,int n,int);
 
+//Function Prototypes
+void problem1();
+void problem2();
+void problem3();
+void problem4();
 
 //Execution Begins Here!!!
 int main(int argc, char** argv) {
@@ -313,5 +313,108 @@ int main(int argc, char** argv) {
                 break;
          */
         
+        /*
+             //Declare menu variable choice
+    char choice;
+    //Repeat the menu
+    do{
+        //General Menu Format
+        //Display the selection
+        cout<<"Type 1 to solve problem 1"<<endl;
+        cout<<"Type 2 to solve problem 2"<<endl;
+        cout<<"Type 3 to solve problem 3"<<endl;
+        cout<<"Type 4 to solve the Cable Bill"<<endl;
+        cout<<"Type anything else to quit with no solutions."<<endl;
+        //Read the choice
+        cin>>choice;
+        //Solve a problem that has been chosen.
+        switch(choice){
+            case '1':problem1();break;
+            case '2':problem2();break;
+            case '3':problem3();break;
+            case '4':problem4();break;
+            default :cout<<"Exit?"<<endl;
+        };
+    }while(choice>='1'&&choice<='4');
+    //Exit Stage right
     return 0;
+}
+         */
+        
+    return 0;
+}
+
+//*******************         Problem 1           ********************/
+void problem1(){
+    int sum=1+2+3+4+5+6+7+8+9+10;
+    cout<<endl;
+    cout<<"Solution to 1 sum(1->10)="<<sum<<endl;
+    cout<<endl;
+}
+
+//*******************         Problem 2           ********************/
+void problem2(){
+    int prod=1*2*3*4*5*6*7*8*9*10;
+    cout<<endl;
+    cout<<"Solution n!(1->10)="<<prod<<endl;
+    cout<<endl;
+}
+
+//*******************         Problem 3           ********************/
+void problem3(){
+    float quot=1.0f/2/3/4/5/6/7/8/9/10;
+    cout<<endl;
+    cout<<"Solution to 3 quot(1->10)="<<quot<<endl;
+    cout<<endl;
+}
+
+//*******************         Problem 4           ********************/
+void problem4(){
+    //Declare Variables
+    ofstream out;//Output the results in a file
+    float vwdHrs;//Hours viewed (hrs)
+    char package;//Package AaBbCc
+    const int SIZE=40;//Max size of name = 39 characters
+    char name[SIZE];//Customer Name
+    float bill;//Cable Bill in $'s
+    //Open the file
+    out.open("Cable.dat");
+    //Prompt the user for inputs
+    cout<<"How many hours did you view this month?"<<endl;
+    cin>>vwdHrs;
+    cout<<"What is your package A,B,C"<<endl;
+    cin>>package;
+    cout<<"What is the customers name?"<<endl;
+    cin.ignore();
+    cin.getline(name,SIZE);
+    //Calculate the paycheck
+    switch(package){
+        case 'A':
+        case 'a':{
+            bill=9.95;
+            if(vwdHrs>10)bill+=2*(vwdHrs-10);
+            break;
+        }
+        case 'B':
+        case 'b':{
+            bill=14.95;
+            if(vwdHrs>20)bill+=(vwdHrs-20);
+            break;
+        }
+        case 'C':
+        case 'c':{
+            bill=19.95;
+            break;
+        }
+        default:  cout<<"Wrong Package Chosen"<<endl;
+    };
+    //Output the results to the file
+    out<<fixed<<setprecision(2)<<showpoint;
+    out<<"Your Cable Bill From CSC5 RCC Programming Class"<<endl;
+    out<<"Customer Name: "<<name<<endl;
+    out<<"Package: "<<package<<endl;
+    out<<"Hours Viewed: "<<vwdHrs<<endl;
+    out<<"Amount Owed: $"<<bill<<endl;
+    //Close the file
+    out.close();
 }
